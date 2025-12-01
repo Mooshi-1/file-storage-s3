7d684e1098897cfbc1ae407b0a9569f9555a58dc
ADG <achet92@gmail.com>
2025-11-30 21:37:45 -0500

    update thumbnail handler to create file on disk rather than store encoded string in db

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -1,10 +1,12 @@
 package main
 
 import (
-	"encoding/base64"
 	"fmt"
 	"io"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
@@ -34,7 +36,6 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 
 	const maxMemory int = (10 << 20)
 	r.ParseMultipartForm(int64(maxMemory))
-	contenttype := r.Header.Get("Content-Type")
 
 	file, header, err := r.FormFile("thumbnail")
 	if err != nil {
@@ -43,11 +44,13 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 	}
 	defer file.Close()
 
-	bytes, err := io.ReadAll(file)
-	if err != nil {
-		respondWithError(w, http.StatusBadRequest, "unable to parse file", err)
-		return
-	}
+	contenttype := header.Header.Get("Content-Type")
+	split := strings.TrimPrefix(contenttype, "image/")
+	// bytes, err := io.ReadAll(file)
+	// if err != nil {
+	// 	respondWithError(w, http.StatusBadRequest, "unable to parse file", err)
+	// 	return
+	// }
 
 	video, err := cfg.db.GetVideo(videoID)
 	if err != nil {
@@ -64,8 +67,25 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 	// 	mediaType: contenttype,
 	// }
 
-	encodedData := base64.StdEncoding.EncodeToString(bytes)
-	dataURL := fmt.Sprintf("data:%v;base64,%v", contenttype, encodedData)
+	//encodedData := base64.StdEncoding.EncodeToString(bytes)
+	// dataURL := fmt.Sprintf("data:%v;base64,%v", contenttype, encodedData)
+	//video.ThumbnailURL = &dataURL
+
+	filepath := filepath.Join(cfg.assetsRoot, fmt.Sprintf("%v.%v", video.ID, split))
+
+	f, err := os.Create(
+		//fmt.Sprintf("./assets/%v.%v", video.ID, split)
+		filepath)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "unable to create file", err)
+		return
+	}
+	_, err = io.Copy(f, file)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "cannot copy file", err)
+		return
+	}
+	dataURL := fmt.Sprintf("http://localhost:%v/assets/%v.%v", cfg.port, video.ID, split)
 	video.ThumbnailURL = &dataURL
 
 	// newURL := fmt.Sprintf("http://localhost:%v/api/thumbnails/%v", cfg.port, video.ID)
@@ -77,7 +97,7 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-	w.Header().Add("thumbnail", header.Filename)
+	w.Header().Add("thumbnail", dataURL)
 
 	respondWithJSON(w, http.StatusOK, video)
 }
